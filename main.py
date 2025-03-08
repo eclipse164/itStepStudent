85bcd9cfde17a0f2986e56139247c274638ae804
User <vitaliyxxxl2012@gmail.com>
2025-03-08 14:50:29 +0200

    calculator with prints

--- main.py
+++ main.py
@@ -3,3 +3,26 @@ print("fghg")
 a = 5
 print(a+4)
 print("edited by another user")
+
+
+
+def calc(num1, num2, operation):
+    if operation == "+":
+        return num1+num2
+    elif operation == "-":
+        return num1-num2
+    elif operation == "/":
+        return num1/num2
+    elif operation == "*":
+        return num1*num2
+
+
+a = int(input())
+b = int(input())
+op = input()
+print(calc(a,b,op))
+
+print(calc(10, 15, "+"))
+print(calc(10, 15, "-"))
+print(calc(10, 15, "/"))
+print(calc(10, 15, "*"))
